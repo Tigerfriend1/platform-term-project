150b0faa0f5c4ef208e0acea1eb25ac64e69b7df
Gamchan Kang <gsgh3016@gmail.com>
2023-09-26 18:48:36 +0900

    test: hello world 코드 작성

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # platform-term-project
 
-Tigerfriend1 : https://github.com/Tigerfriend1/platform-term-project
+Tigerfriend1 : https://github.com/Tigerfriend1
+gsgh3016: https://github.com/gsgh3016
---
+++ src/test/java/com/example/platformtermproject/PlatformTermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platformtermproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformTermProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
